adefdce7c1fc7820c9425f5c36e33aba5eed12b6
Subrat Kumar Das <business.ttrc@gmail.com>
2022-02-10 19:54:08 +0530

    initial commit

---
+++ BSTApp.java
@@ -0,0 +1,61 @@
+package bst;
+
+class Node {
+	  Node left;
+	  Node right;
+	  int data;
+	}
+
+class BST {
+  public void inorder(Node node) {
+	  // Print Root Node in middle
+	    if(node == null) {
+	      return;
+	    }
+	    
+	    inorder(node.left);
+	    System.out.print(node.data + " ");
+	    inorder(node.right);
+	  }
+	  
+  public Node insert(Node node, int val) {
+    if(node == null) {
+      return createNewNode(val);
+    }
+    
+    if(val < node.data) {
+      node.left = insert(node.left, val);
+    } else if((val > node.data)) {
+      node.right = insert(node.right, val);
+    }
+    
+    return node;
+  }
+  
+  public Node createNewNode(int k) {
+    Node a = new Node();
+    a.data = k;
+    a.left = null;
+    a.right = null;
+    return a;
+  }
+}
+public class BSTApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    BST a = new BST();
+	    Node root = null;
+	    
+	    root = a.insert(root, 8);
+	    root = a.insert(root, 3);
+	    root = a.insert(root, 6);
+	    root = a.insert(root, 10);
+	    root = a.insert(root, 4);
+	    root = a.insert(root, 7);
+	    root = a.insert(root, 1);
+	    root = a.insert(root, 14);
+	    root = a.insert(root, 13);
+	    a.inorder(root);
+	}
+}
